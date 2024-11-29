30171f49d7acb4c0e810ff9e73a0b68c090f04bc
lazarus0320 <astar5327z@gmail.com>
2024-11-29 15:54:23 +0900

    Feat: mongo converter 추가

---
+++ src/main/java/org/example/productadminservice/common/config/MongoConfig.java
@@ -0,0 +1,14 @@
+package org.example.productadminservice.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+@Configuration
+public class MongoConfig {
+	@Bean
+	public MongoCustomConversions mongoCustomConversions() {
+		return MongoCustomConversions.create(config ->
+			config.registerConverter(new BinaryToStringConverter()));
+	}
+}
